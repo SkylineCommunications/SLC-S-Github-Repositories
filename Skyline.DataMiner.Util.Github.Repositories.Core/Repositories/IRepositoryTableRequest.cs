f16ea07d10c327cebfdd71090bcc5bdbdf892009
Arne Maes <arne.maes@skyline.be>
2024-02-07 09:19:22 +0100

    Initial version

---
+++ Skyline.DataMiner.Util.Github.Repositories.Core/Repositories/IRepositoryTableRequest.cs
@@ -0,0 +1,57 @@
+﻿// Ignore Spelling: Utils Github dataminer
+
+namespace Skyline.DataMiner.Utils.Github.Repositories.Core.Repositories
+{
+	/// <summary>
+	/// Enum of different action that can be performed on the repositories table of an element.
+	/// </summary>
+	public enum RepositoryTableAction
+	{
+		/// <summary>
+		/// Action to add a new repository to an element.
+		/// </summary>
+		Add,
+
+		/// <summary>
+		/// Action to remove a repository from an element.
+		/// </summary>
+		Remove,
+	}
+
+	/// <summary>
+	/// Enum to indicate whether or not the request needs to be performed organizational wide, or repository wide.
+	/// </summary>
+	public enum IndividualOrOrganization
+	{
+		/// <summary>
+		/// Indicates that the request should be considered repository wide.
+		/// </summary>
+		Individual,
+
+		/// <summary>
+		/// Indicates that the request should be considered organizational wide.
+		/// </summary>
+		Organization,
+	}
+
+	/// <summary>
+	/// A request object for the repositories table, that contains all the information to handle request.
+	/// </summary>
+	public interface IRepositoriesTableRequest
+	{
+		/// <summary>
+		/// The action that needs to be performed on the table.
+		/// </summary>
+		RepositoryTableAction Action { get; }
+
+		/// <summary>
+		/// The scope of the request.
+		/// </summary>
+		IndividualOrOrganization Organization { get; }
+
+		/// <summary>
+		/// The data needed to execute the request.
+		/// </summary>
+		string[] Data { get; }
+	}
+}
---
+++ Skyline.DataMiner.Util.Github.Repositories.Core/Workflows/IWorkflowTableRequest.cs
@@ -0,0 +1,62 @@
+﻿// Ignore Spelling: Utils Github Workflows Workflow
+
+namespace Skyline.DataMiner.Utils.Github.Repositories.Core.Workflows
+{
+	using System.ComponentModel;
+
+	/// <summary>
+	/// The type of workflow.
+	/// </summary>
+	public enum WorkflowType
+	{
+		/// <summary>
+		/// A DataMiner Automation Script CI Workflow.
+		/// </summary>
+		[Description("Automation Script CI")]
+		AutomationScriptCI = 0,
+
+		/// <summary>
+		/// A DataMiner Automation Script CICD Workflow.
+		/// </summary>
+		[Description("Automation Script CICD")]
+		AutomationScriptCICD = 1,
+
+		/// <summary>
+		/// A DataMiner Connector CI Workflow.
+		/// </summary>
+		[Description("Connector CI")]
+		ConnectorCI = 2,
+	}
+
+	/// <summary>
+	/// The type of action that needs to be performed.
+	/// </summary>
+	public enum WorkflowAction
+	{
+		/// <summary>
+		/// Action to add a new workflow to a repository.
+		/// </summary>
+		Add = 0,
+	}
+
+	/// <summary>
+	/// A request object for the workflows table, that contains all the information to handle request.
+	/// </summary>
+	public interface IWorkflowsTableRequest
+	{
+		/// <summary>
+		/// The full id of the repository in the following format: {owner}/{name}.
+		/// </summary>
+		string RepositoryId { get; }
+
+		/// <summary>
+		/// The action that needs to be performed on the table.
+		/// </summary>
+		WorkflowAction Action { get; }
+
+		/// <summary>
+		/// The type of workflow that needs to be added to the repository.
+		/// </summary>
+		WorkflowType WorkflowType { get; }
+	}
+}
